13d2d2de3f935614e61394588d61caaf957f0722
=Minasie Lemma <=m2017info@gmail.com>
2024-06-02 00:20:31 +0300

    fixed bug

--- djongo/features.py
+++ djongo/features.py
@@ -1,4 +1,3 @@
-
 from django.db.backends.base.features import BaseDatabaseFeatures
 
 
@@ -10,3 +9,6 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     supports_timezones = False
     uses_savepoints = False
     can_clone_databases = True
+    test_db_allows_multiple_connections = False
+    supports_unspecified_pk = True
+
